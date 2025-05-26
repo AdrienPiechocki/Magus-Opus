659f285e0f05f6dea039ec5092bef27318022149
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2025-05-26 14:15:56 +0200

    NPC sprite management done

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,1 +1,12 @@
 extends CharacterBody3D
+
+@onready var head = $head
+
+@export_range(-180, 180) var orientation:float
+var spin:float = 0
+
+func _physics_process(delta: float) -> void:
+	spin += 100 * delta
+	if spin > 180:
+		spin = -180
+	head.rotation_degrees.y = orientation
